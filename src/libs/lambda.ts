02412f225e2e632b7df165b492d6fb6d6d81c67c
Ivan Shastakou <ivan_shastakou2@epam.com>
2023-02-23 13:56:22 +0100

    task-3: Handled cors issue

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -2,7 +2,11 @@ import type { Handler } from 'aws-lambda';
 import middy from '@middy/core';
 import middyJsonBodyParser from '@middy/http-json-body-parser';
 import httpHeaderNormalizer from '@middy/http-header-normalizer';
+import cors from '@middy/http-cors';
 
 export const middyfy = (handler: Handler<any>) => {
-  return middy(handler).use(httpHeaderNormalizer()).use(middyJsonBodyParser());
+  return middy(handler)
+    .use(httpHeaderNormalizer())
+    .use(middyJsonBodyParser())
+    .use(cors());
 };
