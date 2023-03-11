f976793300b6b5056142949bd53e04a139aa61b4
Ivan Shastakou <ivan_shastakou2@epam.com>
2023-03-11 19:08:27 +0100

    task-5: Moved uploaded file after processing

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -2,14 +2,20 @@ import { pipeline } from 'node:stream/promises';
 import { S3Event } from 'aws-lambda';
 import { InternalServerError } from 'http-errors';
 import csvParser from 'csv-parser';
-import { getObject } from '@libs/services/s3Client.service';
+import {
+  copyObject,
+  deleteObject,
+  getObject,
+} from '@libs/services/s3Client.service';
+import { BUCKET_PARSED_PREFIX } from '@libs/constants';
 
 export const importFileParser = async (event: S3Event) => {
   const objectMetadata = event.Records?.[0]?.s3?.object;
+  const objectKey = objectMetadata.key;
 
   try {
     const chunks = [];
-    const objectStream = await getObject(objectMetadata.key);
+    const objectStream = await getObject(objectKey);
 
     await pipeline(objectStream, csvParser(), async function* (source) {
       for await (const chunk of source) {
@@ -17,6 +23,11 @@ export const importFileParser = async (event: S3Event) => {
         yield chunks.push(chunk);
       }
     });
+
+    const copiedObjectKey = BUCKET_PARSED_PREFIX + objectKey.split('/').pop();
+    await copyObject(objectKey, copiedObjectKey);
+
+    await deleteObject(objectKey);
   } catch (error) {
     console.error(error);
     throw new InternalServerError(error);
--- import-service/src/libs/services/s3Client.service.ts
+++ import-service/src/libs/services/s3Client.service.ts
@@ -2,19 +2,47 @@ import {
   PutObjectCommand,
   S3Client,
   GetObjectCommand,
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  DeleteBucketCommandOutput,
+  CopyObjectCommandOutput,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { BUCKET, REGION } from '../constants';
 
 const s3Client = new S3Client({ region: REGION });
 
-export const createPresignedUrl = async (key: string): Promise<string> => {
+export async function createPresignedUrl(key: string): Promise<string> {
   const command = new PutObjectCommand({ Bucket: BUCKET, Key: key });
   return getSignedUrl(s3Client, command, { expiresIn: 3600 });
-};
+}
 
 export async function getObject(key: string): Promise<NodeJS.ReadableStream> {
   const command = new GetObjectCommand({ Bucket: BUCKET, Key: key });
   const result = await s3Client.send(command);
   return result.Body as NodeJS.ReadableStream;
+}
+
+export async function copyObject(
+  sourceKey: string,
+  destinationKey: string
+): Promise<CopyObjectCommandOutput> {
+  const command = new CopyObjectCommand({
+    Bucket: BUCKET,
+    CopySource: `${BUCKET}/${sourceKey}`,
+    Key: destinationKey,
+  });
+  const result = await s3Client.send(command);
+  return result;
+}
+
+export async function deleteObject(
+  key: string
+): Promise<DeleteBucketCommandOutput> {
+  const command = new DeleteObjectCommand({
+    Bucket: BUCKET,
+    Key: key,
+  });
+  const result = await s3Client.send(command);
+  return result;
 }
