0361b6758a163d4d21117a13512ad57e1779822c
Ivan Shastakou <ivan_shastakou2@epam.com>
2023-03-11 19:08:27 +0100

    task-5: Moved uploaded file after processing

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -2,24 +2,40 @@ import { pipeline } from 'node:stream/promises';
 import { S3Event } from 'aws-lambda';
 import { InternalServerError } from 'http-errors';
 import csvParser from 'csv-parser';
-import { getObject } from '@libs/services/s3Client.service';
+import {
+  copyObject,
+  deleteObject,
+  getObject,
+} from '@libs/services/s3Client.service';
+import { BUCKET_PARSED_PREFIX } from '@libs/constants';
 
 export const importFileParser = async (event: S3Event) => {
-  const objectMetadata = event.Records?.[0]?.s3?.object;
+  const s3Records = event.Records ?? [];
 
-  try {
-    const chunks = [];
-    const objectStream = await getObject(objectMetadata.key);
+  for (const s3Record of s3Records) {
+    const objectMetadata = s3Record?.s3?.object;
+    const objectKey = objectMetadata.key;
 
-    await pipeline(objectStream, csvParser(), async function* (source) {
-      for await (const chunk of source) {
-        console.log(chunk);
-        yield chunks.push(chunk);
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    throw new InternalServerError(error);
+    try {
+      const parsedProducts = [];
+      const objectStream = await getObject(objectKey);
+
+      await pipeline(objectStream, csvParser(), async function* (parsedLines) {
+        for await (const parsedProduct of parsedLines) {
+          console.log(parsedProduct);
+          parsedProducts.push(parsedProduct);
+          yield parsedProduct;
+        }
+      });
+
+      const copiedObjectKey = BUCKET_PARSED_PREFIX + objectKey.split('/').pop();
+      await copyObject(objectKey, copiedObjectKey);
+
+      await deleteObject(objectKey);
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerError(error);
+    }
   }
 };
 
--- import-service/src/libs/constants/index.ts
+++ import-service/src/libs/constants/index.ts
@@ -2,3 +2,4 @@ export const REGION = process.env.REGION;
 export const BUCKET = process.env.BUCKET;
 export const BUCKET_EVENT = 's3:ObjectCreated:*';
 export const BUCKET_UPLOADED_PREFIX = 'uploaded/';
+export const BUCKET_PARSED_PREFIX = 'parsed/';
